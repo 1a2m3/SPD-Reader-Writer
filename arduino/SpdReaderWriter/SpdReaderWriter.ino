e7a684b1378b68438613cc89c005addca88feb4b
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2020-07-20 02:40:12 -0700

    PSWP added
    Added PSWP function and moved settings to separate file (SpdReaderWriterSettings.h)

--- arduino/SpdReaderWriter/SpdReaderWriter.ino
+++ arduino/SpdReaderWriter/SpdReaderWriter.ino
@@ -4,35 +4,42 @@
 */
 
 #include <Wire.h>
-#define SERIAL_BAUD_RATE 115200
+#include "SpdReaderWriterSettings.h" // Settings
 
 // EEPROM commands
-#define SPA0 0x36  // Set EE Page Address to 0
-#define SPA1 0x37  // Set EE Page Address to 1
+#define SPA0 0x36   // Set EE Page Address to 0 (DDR4 only)
+#define SPA1 0x37   // Set EE Page Address to 1 (DDR4 only)
 
 // RSWP commands
-#define SWP0 0x31   // Set Write Protection for block 0 (addresses  00h to  7Fh) (  0-127)
-#define SWP1 0x34   // Set Write Protection for block 1 (addresses  80h to  FFh) (128-255)
-#define SWP2 0x35   // Set Write Protection for block 2 (addresses 100h to 17Fh) (256-383)  
-#define SWP3 0x30   // Set Write Protection for block 3 (addresses 180h to 1FFh) (384-511)
+#define SWP0 0x31   // Set Write Protection for block 0 (addresses  00h to  7Fh) (  0-127) (DDR4 only)
+#define SWP1 0x34   // Set Write Protection for block 1 (addresses  80h to  FFh) (128-255) (DDR4 only)
+#define SWP2 0x35   // Set Write Protection for block 2 (addresses 100h to 17Fh) (256-383) (DDR4 only)
+#define SWP3 0x30   // Set Write Protection for block 3 (addresses 180h to 1FFh) (384-511) (DDR4 only)
 #define CWP  0x33   // Clear Write Protection for all 4 blocks
+#define DNC  0x00   // "Do not care" byte
+#define PWPB 0b0110 // PSWP command bits (7-4)
 
-#define HVSW 6      // High Voltage (Optocoupler) switch pin
-#define PORT Serial // Communications Port, change to "SerialUSB" for native USB Arduinos (Leonardo, Micro, Due, Yun, etc)
-
-int eeAddress = 0;  // EE Page address
+int eeAddress = 0;  // Initial EEPROM page address
 
 void setup() {
 
-  pinMode(HVSW, OUTPUT);
+  pinMode(HVSW,  OUTPUT);
+  pinMode(SA0SW, OUTPUT);
+  pinMode(SA1SW, OUTPUT);
+  pinMode(SA2SW, OUTPUT);
 
   Wire.begin();
   setPageAddress(eeAddress); // Reset eeprom page address
 
-  PORT.begin(SERIAL_BAUD_RATE);
+  PORT.begin(BAUD_RATE);
   PORT.setTimeout(10000); // 10 sec. timeout
 
   while (!PORT) {} // Wait for serial monitor
+}
+
+// Turns on or off address select pins
+void setAddressPin(int pin, bool state) {
+  digitalWrite(pin, state);
 }
 
 // Reverses software write protection
@@ -42,8 +49,8 @@ bool clearWriteProtection() {
   delay(10);
 
   Wire.beginTransmission(CWP);
-  Wire.write(0x00); // "Dummy" writes
-  Wire.write(0x00); // Works without them, but PDFs say these are needed
+  Wire.write(DNC);
+  Wire.write(DNC);
   int status = Wire.endTransmission();
   delay(10);
 
@@ -56,18 +63,31 @@ bool clearWriteProtection() {
 bool setWriteProtection(uint8_t block) {
 
   int commands[] = {SWP0, SWP1, SWP2, SWP3};
-  int cmd = (block >= 0 || block <= 3) ? commands[block] : SWP0;
+  int cmd = (block >= 0 || block <= 3) ? commands[block] : commands[0];
 
   digitalWrite(HVSW, HIGH); // Turn on the optocoupler
   delay(10);
 
   Wire.beginTransmission(cmd);
-  Wire.write(0x00); // "Dummy" writes
-  Wire.write(0x00); // Works without them, but PDFs say these are needed
+  Wire.write(DNC);
+  Wire.write(DNC);
   int status = Wire.endTransmission(); // status == 0 when SWP is enabled
   delay(10);
 
   digitalWrite(HVSW, LOW); // Turn off the optocoupler
+
+  return status == 0;
+}
+
+// Sets permanent write protection on supported EEPROMs
+bool setPermanentWriteProtection(uint8_t deviceAddress) {
+
+  uint8_t cmd = (byte)(deviceAddress & 0b111) + (PWPB << 4); // Keep address bits (SA0-SA2) intact and change bits 7-4 to '0110'
+  Wire.beginTransmission(cmd); 
+  Wire.write(DNC);
+  Wire.write(DNC);
+  int status = Wire.endTransmission();
+  delay(10);
 
   return status == 0;
 }
@@ -223,6 +243,25 @@ void cmdEnableWP() {
   PORT.write((byte)1); // Error
 }
 
+void cmdEnablePSWP() {
+  int address = PORT.parseInt(); // Device address
+
+  if (setPermanentWriteProtection(address)) {
+    PORT.write((byte)0); // Success
+    return;
+  }
+  PORT.write((byte)1); // Error
+}
+
+void cmdSetAddress() {
+  int address = PORT.parseInt();
+
+  setAddressPin(SA0SW, (address >> 0) & 0b1);
+  setAddressPin(SA1SW, (address >> 1) & 0b1);
+  setAddressPin(SA2SW, (address >> 2) & 0b1);
+
+}
+
 void parseCommand() {
   if (!PORT.available()) {
     return;
@@ -231,19 +270,23 @@ void parseCommand() {
   char cmd = PORT.read();
 
   switch (cmd) {
-    case 't': cmdTest();      // Device Communication Test
+    case 't': cmdTest();       // Device Communication Test
       break;
-    case 's': cmdScan();      // Scan i2c bus
+    case 's': cmdScan();       // Scan i2c bus
       break;
-    case 'r': cmdRead();      // Read byte
+    case 'r': cmdRead();       // Read byte
       break;
-    case 'w': cmdWrite();     // Write byte
+    case 'w': cmdWrite();      // Write byte
       break;
-    case 'p': cmdProbe();     // Probe i2c address
+    case 'p': cmdProbe();      // Probe i2c address
       break;
-    case 'c': cmdClearWP();   // Clear reversible software write protection
+    case 'c': cmdClearWP();    // Clear reversible software write protection
       break;
-    case 'e': cmdEnableWP();  // Enable write protection
+    case 'e': cmdEnableWP();   // Enable write protection
+      break;
+    case 'x': cmdEnablePSWP(); // Enable permanent write protection
+      break;
+    case 'a': cmdSetAddress(); // Set EEPROM address
       break;
   }
 }
