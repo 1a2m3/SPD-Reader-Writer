a780a72d81674d2feb25cad2fa4c4356eb1e5915
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2020-07-31 05:01:15 -0700

    Stability, performance, and new features
    Improved stability and performance, added new features. Requires Firmware specified in Settings class.

---
+++ src/SpdReaderWriterDll/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpdReaderWriterDll {
+	
+	/// <summary>
+	/// Configurable settings
+	/// </summary>
+	public class Settings {
+
+		/// <summary>
+		/// Serial port baud rate
+		/// </summary>
+		public const int BAUDRATE = 115200; //115200;
+
+		/// <summary>
+		/// DLL version
+		/// </summary>
+		public const int VERSION = 20200731;
+
+		/// <summary>
+		/// Minimum device's firmware version required 
+		/// </summary>
+		public static int MinimumVersionRequired = 20200731;
+	}
+}
