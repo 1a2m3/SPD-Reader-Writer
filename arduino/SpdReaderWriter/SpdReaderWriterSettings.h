900bd6b1186f389a492b03fc7177e1e2a5e20d5a
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2020-08-08 16:02:34 -0700

    Delete SpdReaderWriterSettings.h

--- arduino/SpdReaderWriter/SpdReaderWriterSettings.h
+++
@@ -1,6 +0,0 @@
-#define PORT      Serial   // Communications Port, default is "Serial". Change to "SerialUSB" for native USB Arduinos (Leonardo, Micro, Due, Yun, etc)
-#define BAUD_RATE 115200   // Serial port baud rate, must match program's serial baud rate
-#define HVSW      6        // High Voltage (Optocoupler) switch pin number
-#define SA0SW     2        // SA0 select pin number
-#define SA1SW     3        // SA1 select pin number
-#define SA2SW     4        // SA2 select pin number
