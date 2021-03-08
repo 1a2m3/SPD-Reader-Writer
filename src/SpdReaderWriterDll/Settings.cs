b334b7925748cb27378247ee76db07c15330415c
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2021-03-07 16:51:51 -0800

    Removed baud rate and simplified FW requiremet
    Baud rate can now be specified programmatically, and firmware requirements has been downgraded, because differences between 20210204 and 20210212 were cosmetic

--- src/SpdReaderWriterDll/Settings.cs
+++ src/SpdReaderWriterDll/Settings.cs
@@ -8,18 +8,13 @@ namespace SpdReaderWriterDll {
     public class Settings {
 
         /// <summary>
-        /// Serial port baud rate
-        /// </summary>
-        public const int BAUDRATE = 115200; //115200;
-
-        /// <summary>
         /// DLL version
         /// </summary>
-        public const int DLLVERSION = 20210228;
+        public const int DLLVERSION = 20210307;
 
         /// <summary>
         /// Minimum device's firmware version required 
         /// </summary>
-        public static int MINVERSION = 20210212;
+        public static int MINVERSION = 20210204;
     }
 }
