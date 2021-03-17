49fe1333465ed6e6f1dbb5304d522cc5158742d0
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2021-03-17 15:38:29 -0700

    Update and rename app.config to Settings.cs

---
+++ src/SpdReaderWriterDll/Settings.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace SpdReaderWriterDll {
+
+    /// <summary>
+    /// Application configuration
+    /// </summary>
+    public class Settings {
+
+        /// <summary>
+        /// DLL version
+        /// </summary>
+        public const int DLLVERSION  = 20210314;
+
+        /// <summary>
+        /// Minimum device's firmware version required 
+        /// </summary>
+        public static int MINVERSION = 20210313;
+    }
+}
