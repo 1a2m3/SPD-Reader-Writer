f9df47379dd1b53302f3376511e5a0cb693ea79f
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2021-03-25 03:03:31 -0700

    Update Settings.cs

--- src/SpdReaderWriterDll/Settings.cs
+++ src/SpdReaderWriterDll/Settings.cs
@@ -10,7 +10,7 @@ namespace SpdReaderWriterDll {
         /// <summary>
         /// DLL version
         /// </summary>
-        public const int DLLVERSION  = 20210323;
+        public const int DLLVERSION  = 20210325;
 
         /// <summary>
         /// Minimum device's firmware version required 
