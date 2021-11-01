e83f0ccd83bf3aa43d864776787e6eec1a379f5e
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2021-10-31 17:50:44 -0700

    Update SpdReaderWriterSettings.h

--- firmware/SpdReaderWriterSettings.h
+++ firmware/SpdReaderWriterSettings.h
@@ -1,6 +1,6 @@
 /*
     Arduino based EEPROM SPD reader and writer
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    For overclockers and PC hardware enthusiasts
 
    Repos:   https://github.com/1a2m3/SPD-Reader-Writer
@@ -12,13 +12,15 @@
 */
 
 /* -= Communication settings =- */
-#define PORT        Serial   // Communications Port, default is "Serial". Change to "SerialUSB" for native USB Arduinos (Leonardo, Micro, Due, Yun, etc)
-#define BAUD_RATE   115200   // Serial port baud rate, must match program's serial baud rate
-#define I2CCLOCK    100000   // I2C communication clock frequency, default value is 100000. Set to 400000 to enable Fast Mode. Some processors also support 10000 (low speed mode), 1000000 (fast mode plus) and 3400000 (high speed mode). 
+#define PORT        Serial               // Communications Port, default is "Serial". Change to "SerialUSB" for native USB Arduinos (Leonardo, Micro, Due, Yun, etc)
+#define BAUD_RATE   115200               // Serial port baud rate, default value is "115200". Must match program's serial baud rate.
+#define I2C_CLOCK   100000               // I2C communication clock frequency, default value is "100000". Set to "400000" to enable Fast Mode.
 
 /* -= Pins config =- */
-#define HVSW         9       // High Voltage (9V) switch pin number
-#define HVDET        6       // High Voltage (9V) detector pin number
-#define SA0SW       A0       // SA0 select pin number
-#define SA1SW       A1       // SA1 select pin number
-#define SA2SW       A2       // SA2 select pin number
+#define HV_SW        9                   // High Voltage (9V) switch pin    (DDR4/DDR3/DDR2)
+#define HV_FB        6                   // High Voltage (9V) feedback pin  (DDR4/DDR3/DDR2)
+#define OFF_SW      A0                   // Offline mode switch pin         (DDR5)
+#define SA1_SW      A1                   // SA1 switch pin                  (DDR3/DDR2)
+
+/* -= RAM support =- */
+#define RAM_SUPPORT DDR2|DDR3|DDR4|DDR5  // Enter pipe-separated RAM defines to enable specific RAM support. Default value is "DDR2|DDR3|DDR4|DDR5". (SDRAM and DDR are supported by default). To leave SDRAM and DDR support only, enter "DEFAULT"
