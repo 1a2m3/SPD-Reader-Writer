0acead3909d695dbf4e0d84092b636db13178106
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2021-11-02 01:45:06 -0700

    Update SpdReaderWriterSettings.h

--- firmware/SpdReaderWriterSettings.h
+++ firmware/SpdReaderWriterSettings.h
@@ -12,7 +12,7 @@
 */
 
 /* -= Communication settings =- */
-#define PORT        Serial               // Communications Port, default is "Serial". Change to "SerialUSB" for native USB Arduinos (Leonardo, Micro, Due, Yun, etc)
+#define PORT        Serial               // Communications Port, default is "Serial". Change to "SerialUSB" for native USB Arduinos.
 #define BAUD_RATE   115200               // Serial port baud rate, default value is "115200". Must match program's serial baud rate.
 #define I2C_CLOCK   100000               // I2C communication clock frequency, default value is "100000". Set to "400000" to enable Fast Mode.
 
@@ -21,6 +21,3 @@
 #define HV_FB        6                   // High Voltage (9V) feedback pin  (DDR4/DDR3/DDR2)
 #define OFF_SW      A0                   // Offline mode switch pin         (DDR5)
 #define SA1_SW      A1                   // SA1 switch pin                  (DDR3/DDR2)
-
-/* -= RAM support =- */
-#define RAM_SUPPORT DDR2|DDR3|DDR4|DDR5  // Enter pipe-separated RAM defines to enable specific RAM support. Default value is "DDR2|DDR3|DDR4|DDR5". (SDRAM and DDR are supported by default). To leave SDRAM and DDR support only, enter "DEFAULT"
