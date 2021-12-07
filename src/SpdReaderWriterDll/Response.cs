588792d5cfb423320520af6ed31267ba2306f1c6
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2021-12-06 21:10:41 -0800

    Consistency and cleanup

--- src/SpdReaderWriterDll/Response.cs
+++ src/SpdReaderWriterDll/Response.cs
@@ -4,14 +4,6 @@ namespace SpdReaderWriterDll {
     /// </summary>
     public struct Response {
         /// <summary>
-        /// Indicates the operation was executed successfully
-        /// </summary>
-        public const byte SUCCESS = 0x01;
-        /// <summary>
-        /// Indicates the operation has failed
-        /// </summary>
-        public const byte ERROR   = 0xFF;
-        /// <summary>
         /// Boolean True response
         /// </summary>
         public const byte TRUE    = 0x01;
@@ -20,12 +12,20 @@ namespace SpdReaderWriterDll {
         /// </summary>
         public const byte FALSE   = 0x00;
         /// <summary>
+        /// Indicates the operation has failed
+        /// </summary>
+        public const byte ERROR   = 0xFF;
+        /// <summary>
+        /// Indicates the operation was executed successfully
+        /// </summary>
+        public const byte SUCCESS = 0x01;
+        /// <summary>
         /// A response used to indicate an error when normally a numeric non-zero answer is expected if the operation was executed successfully
         /// </summary>
         public const byte NULL    = 0;
         /// <summary>
         /// A response used to describe when SA pin is tied to VCC
-        /// </summary>
+        /// </summary> 
         public const byte ON      = 1;
         /// <summary>
         /// A response used to describe when SA pin is tied to GND
@@ -41,10 +41,12 @@ namespace SpdReaderWriterDll {
         public const char UNKNOWN = '?';
 
         // Aliases
-        public const byte ACK   = SUCCESS;
-        public const byte NACK  = ERROR;
-        public const byte NOACK = ERROR;
-        public const byte FAIL  = ERROR;
-        public const byte ZERO  = NULL;
+        public const byte ACK      = SUCCESS;
+        public const byte ENABLED  = TRUE;
+        public const byte DISABLED = FALSE;
+        public const byte NACK     = ERROR;
+        public const byte NOACK    = ERROR;
+        public const byte FAIL     = ERROR;
+        public const byte ZERO     = NULL;
     }
 }
