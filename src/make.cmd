f748ae8ca57dd3bbe48bac68f96e9a2f8bb8b2c2
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2022-11-17 19:08:45 -0800

    Compiler fix
    Fixed compiler to include SPD classes

--- src/make.cmd
+++ src/make.cmd
@@ -1,4 +1,4 @@
 del *.dll
 del *.exe
-csc.exe /warn:0 /platform:x86 /target:library /out:spdrw.dll /o SpdReaderWriterDll\*.cs
+csc.exe /warn:0 /platform:x86 /target:library /out:spdrw.dll /o SpdReaderWriterDll\*.cs SpdReaderWriterDll\SPD\*.cs
 csc.exe /warn:0 /platform:x86 /target:exe /reference:spdrw.dll /out:spdrwcli.exe /o SpdReaderWriter\*.cs
