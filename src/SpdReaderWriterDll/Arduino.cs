7cbe633a602e833f7dd7c8e329050f77fcb093d6
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2022-12-20 22:22:09 -0800

    FW version info, refactoring
    Added included FW version info, refactoring and cleanup

--- src/SpdReaderWriterDll/Arduino.cs
+++ src/SpdReaderWriterDll/Arduino.cs
@@ -17,6 +17,7 @@ using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace SpdReaderWriterDll {
@@ -413,7 +414,7 @@ namespace SpdReaderWriterDll {
         /// <summary>
         /// Gets current device I2C clock
         /// </summary>
-        public ushort I2CClock => (ushort)(GetI2CClock() ? 400 : 100);
+        public ushort I2CClock => GetI2CClock() ? ClockMode.Fast : ClockMode.Standard;
 
         /// <summary>
         /// Resets the device's settings to defaults
@@ -684,7 +685,7 @@ namespace SpdReaderWriterDll {
                 try {
                     if (IsConnected) {
                         version = int.Parse(
-                            Data.BytesToString(ExecuteCommand(Command.GETVERSION, 8))
+                            Data.BytesToString(ExecuteCommand(Command.GETVERSION, Command.VERSIONLENGTH))
                         );
                     }
                 }
@@ -693,6 +694,32 @@ namespace SpdReaderWriterDll {
                 }
             }
             return version;
+        }
+
+        /// <summary>
+        /// Included firmware version number
+        /// </summary>
+        public static int IncludedFirmwareVersion => GetIncludedFirmwareVersion();
+
+        /// <summary>
+        /// Gets included firmware version number
+        /// </summary>
+        /// <returns>Included firmware version number</returns>
+        private static int GetIncludedFirmwareVersion() {
+            try {
+                byte[] fwHeader = Data.GzipPeek(Properties.Resources.Firmware.SpdReaderWriter_ino, 1024);
+                Regex versionPattern = new Regex($@"([\d]{{{Command.VERSIONLENGTH}}})"); // ([\d]{8})
+                MatchCollection matches = versionPattern.Matches(Data.BytesToString(fwHeader));
+
+                if (matches.Count > 0 && matches[0].Length == Command.VERSIONLENGTH) {
+                    return int.Parse(matches[0].Value);
+                }
+
+                throw new Exception();
+            }
+            catch {
+                throw new Exception($"Unable to get included firmware version number");
+            }
         }
 
         /// <summary>
@@ -1159,83 +1186,108 @@ namespace SpdReaderWriterDll {
             /// <summary>
             /// Read byte
             /// </summary>
-            public const byte READBYTE     = (byte)'r';
+            public const byte READBYTE      = (byte)'r';
+
+
             /// <summary>
             /// Write byte
             /// </summary>
-            public const byte WRITEBYTE    = (byte)'w';
+            public const byte WRITEBYTE     = (byte)'w';
+
             /// <summary>
             /// Write page
             /// </summary>
-            public const byte WRITEPAGE    = (byte)'g';
+            public const byte WRITEPAGE     = (byte)'g';
+
             /// <summary>
             /// Scan i2c bus
             /// </summary>
-            public const byte SCANBUS      = (byte)'s';
+            public const byte SCANBUS       = (byte)'s';
+
             /// <summary>
             /// Set i2c clock 
             /// </summary>
-            public const byte I2CCLOCK     = (byte)'c';
+            public const byte I2CCLOCK      = (byte)'c';
+
             /// <summary>
             /// Probe i2c address
             /// </summary>
-            public const byte PROBEADDRESS = (byte)'a';
+            public const byte PROBEADDRESS  = (byte)'a';
+
             /// <summary>
             /// Config pin state control
             /// </summary>
-            public const byte PINCONTROL   = (byte)'p';
+            public const byte PINCONTROL    = (byte)'p';
+
             /// <summary>
             /// RSWP control
             /// </summary>
-            public const byte RSWP         = (byte)'b';
+            public const byte RSWP          = (byte)'b';
+
             /// <summary>
             /// PSWP control
             /// </summary>
-            public const byte PSWP         = (byte)'l';
+            public const byte PSWP          = (byte)'l';
+
             /// <summary>
             /// Get Firmware version
             /// </summary>
-            public const byte GETVERSION   = (byte)'v';
+            public const byte GETVERSION    = (byte)'v';
+
+            /// <summary>
+            /// Firmware version number length
+            /// </summary>
+            public const byte VERSIONLENGTH = 8;
+
             /// <summary>
             /// Device Communication Test
             /// </summary>
-            public const byte TESTCOMM     = (byte)'t';
+            public const byte TESTCOMM      = (byte)'t';
+
             /// <summary>
             /// Report current RSWP RAM support
             /// </summary>
-            public const byte RSWPREPORT   = (byte)'f';
+            public const byte RSWPREPORT    = (byte)'f';
+
             /// <summary>
             /// Device name controls
             /// </summary>
-            public const byte NAME         = (byte)'n';
+            public const byte NAME          = (byte)'n';
+
             /// <summary>
             /// DDR4 detection
             /// </summary>
-            public const byte DDR4DETECT   = (byte)'4';
+            public const byte DDR4DETECT    = (byte)'4';
+
             /// <summary>
             /// DDR5 detection
             /// </summary>
-            public const byte DDR5DETECT   = (byte)'5';
+            public const byte DDR5DETECT    = (byte)'5';
+
             /// <summary>
             /// Restore device settings to default
             /// </summary>
-            public const byte FACTORYRESET = (byte)'-';
+            public const byte FACTORYRESET  = (byte)'-';
+
             /// <summary>
             /// Suffix added to get current state
             /// </summary>
-            public const byte GET          = (byte)'?';
+            public const byte GET           = (byte)'?';
+
             /// <summary>
             /// Suffix added to set state equivalent to true/on/enable etc
             /// </summary>
-            public const byte ON           = 1;
+            public const byte ON            = 1;
+
             /// <summary>
             /// Suffix added to set state equivalent to false/off/disable etc
             /// </summary>
-            public const byte OFF          = 0;
+            public const byte OFF           = 0;
+
             /// <summary>
             /// "Do not care" byte
             /// </summary>
-            public const byte DNC          = 0;
+            public const byte DNC           = 0;
         }
 
         /// <summary>
@@ -1376,6 +1428,22 @@ namespace SpdReaderWriterDll {
                 /// </summary>
                 public const byte DDR5 = 1 << 5;
             }
+        }
+
+        /// <summary>
+        /// Arduino's I2C bus clock mode
+        /// </summary>
+        public struct ClockMode {
+
+            /// <summary>
+            /// High speed mode (400 kHz)
+            /// </summary>
+            public const ushort Fast     = 400;
+
+            /// <summary>
+            /// Standard mode (100 kHz)
+            /// </summary>
+            public const ushort Standard = 100;
         }
 
         /// <summary>
