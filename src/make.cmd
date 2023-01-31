383a12dc202697c901fff719bc75fd9d19cc2b3d
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2023-01-30 23:56:53 -0800

    Refactoring

--- src/make.cmd
+++ src/make.cmd
@@ -1,4 +1,4 @@
 del *.dll
 del *.exe
-csc.exe /platform:x86 /target:library /out:spdrw.dll /o SpdReaderWriterDll\*.cs SpdReaderWriterDll\SPD\*.cs SpdReaderWriterDll\Driver\*.cs
-csc.exe /platform:x86 /target:exe /reference:spdrw.dll /out:spdrwcli.exe /o SpdReaderWriter\*.cs
+csc.exe /platform:x86 /target:library /out:spdrwcore.dll /o SpdReaderWriterCore\*.cs SpdReaderWriterCore\SPD\*.cs SpdReaderWriterCore\Driver\*.cs
+csc.exe /platform:x86 /target:exe /reference:spdrwcore.dll /out:spdrwcli.exe /o SpdReaderWriter\*.cs
