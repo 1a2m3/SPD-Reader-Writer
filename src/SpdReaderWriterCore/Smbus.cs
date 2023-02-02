ea4bc9258c13e50ecfbb15f46efd982a4fceaa93
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2023-02-02 02:11:11 -0800

    Refactoring, cleanup

--- src/SpdReaderWriterCore/Smbus.cs
+++ src/SpdReaderWriterCore/Smbus.cs
@@ -15,7 +15,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using SpdReaderWriterDll.Driver;
+using SpdReaderWriterCore.Driver;
 
 namespace SpdReaderWriterCore {
 
