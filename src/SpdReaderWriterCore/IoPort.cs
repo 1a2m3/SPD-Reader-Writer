a85e5a50ae5228a972a080e64b22d2939971185f
1a2m3 <65949266+1a2m3@users.noreply.github.com>
2023-09-22 17:52:18 -0700

    Refactoring

--- src/SpdReaderWriterCore/IoPort.cs
+++ src/SpdReaderWriterCore/IoPort.cs
@@ -11,6 +11,7 @@
 
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace SpdReaderWriterCore {
 
@@ -44,7 +45,7 @@ namespace SpdReaderWriterCore {
         /// </summary>
         /// <returns>Readable IO port instance description</returns>
         public override string ToString() {
-            return $"IO port {BaseAddress:X4}h";
+            return $"0x{BaseAddress:X4}";
         }
 
         /// <summary>
@@ -55,24 +56,25 @@ namespace SpdReaderWriterCore {
         /// <returns>Register value</returns>
         public T Read<T>(ushort offset) {
 
-            object output = null;
+            Data.DataSize inputSize = Data.GetDataSize(typeof(T));
 
-            if (typeof(T) == typeof(byte)) {
-                output = Smbus.Driver.ReadIoPortByte((ushort)(BaseAddress + offset));
-            }
-            else if (typeof(T) == typeof(ushort)) {
-                output = Smbus.Driver.ReadIoPortWord((ushort)(BaseAddress + offset));
-            }
-            else if (typeof(T) == typeof(uint)) {
-                output = Smbus.Driver.ReadIoPortDword((ushort)(BaseAddress + offset));
+            if (inputSize > Data.DataSize.Dword || inputSize < Data.DataSize.Byte) {
+                throw new InvalidDataException($"{MethodBase.GetCurrentMethod()?.Name}:{typeof(T)}");
             }
 
-            if (output != null) {
-                return (T)Convert.ChangeType(output, typeof(T));
-            }
-
-            throw new InvalidDataException(nameof(T));
+            object output = KernelDriver.ReadIoPort<T>((ushort)(BaseAddress + offset));
+            return (T)Convert.ChangeType(output, typeof(T));
         }
+
+        /// <summary>
+        /// Reads data from an IO port register
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="offset">Register offset</param>
+        /// <param name="output">Output reference</param>
+        /// <returns><see langword="true"/> if the function succeeds</returns>
+        public bool Read<T>(ushort offset, out T output) => 
+            KernelDriver.ReadIoPortEx(offset, out output);
 
         /// <summary>
         /// Writes data to an IO port register
@@ -80,24 +82,17 @@ namespace SpdReaderWriterCore {
         /// <typeparam name="T">Data type</typeparam>
         /// <param name="offset">Register offset</param>
         /// <param name="value">Data value</param>
-        /// <returns><see lang="true"/> if the function succeeds</returns>
+        /// <returns><see langword="true"/> if the function succeeds</returns>
         public bool Write<T>(ushort offset, T value) {
 
-            object input = Convert.ChangeType(value, typeof(T));
+            object input = value;
+            Data.DataSize inputSize = Data.GetDataSize(input);
 
-            if (typeof(T) == typeof(byte)) {
-                return Smbus.Driver.WriteIoPortByteEx((ushort)(BaseAddress + offset), (byte)input);
+            if (inputSize > Data.DataSize.Dword || inputSize < Data.DataSize.Byte) {
+                throw new InvalidDataException($"{MethodBase.GetCurrentMethod()?.Name}:{typeof(T)}");
             }
 
-            if (typeof(T) == typeof(ushort)) {
-                return Smbus.Driver.WriteIoPortWordEx((ushort)(BaseAddress + offset), (ushort)input);
-            }
-
-            if (typeof(T) == typeof(uint)) {
-                return Smbus.Driver.WriteIoPortDwordEx((ushort)(BaseAddress + offset), (uint)input);
-            }
-
-            throw new InvalidDataException(nameof(T));
+            return KernelDriver.WriteIoPortEx((ushort)(BaseAddress + offset), (T)input);
         }
     }
 }
